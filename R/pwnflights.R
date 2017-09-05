d3248a85bb4569f541ff74fc92eba3fe62ebfc88
Matthew Yancheff <yanchefm@reed.edu>
2017-09-04 23:16:59 -0700

    added survey data and flight data with documentation

--- R/pwnflights.R
+++ R/pwnflights.R
@@ -3,9 +3,8 @@
 #' On-time data for a random sample of flights that departed the PNW (i.e. PDX or SEA)
 #' from JUne 1st to May 31st.
 #'
-#' @source Hadley Wickham (2014) (Update later). \code{nycflights13}: Data about flights departing
-#' NYC in 2013. R package version 0.1. (Update Later)
-#'  \url{https://CRAN.R-project.org/package=nycflights13} (Update Later)
+#' @source RITA, Bureau of transportation statistics,
+#'  \url{http://www.transtats.bts.gov/DL_SelectFields.asp?Table_ID=236}
 #' @format A tbl_df with 193809 rows and 16 variables:
 #' \describe{
 #' \item{year,month,day}{Date of departure.}
@@ -14,11 +13,11 @@
 #'   Negative times represent early departures/arrivals.}
 #' \item{hour,minute}{Time of departure broken in to hour and minutes.}
 #' \item{carrier}{Two letter carrier abbreviation. See \code{airlines} in the
-#'   \code{nycflights13} package for more information or google the airline code.} (Update later)
+#'   \code{nycflights13} package for more information or google the airline code.}
 #' \item{tailnum}{Plane tail number.}
 #' \item{flight}{Flight number.}
 #' \item{origin,dest}{Origin and destination. See \code{airports} in the
-#'   \code{nycflights13} package for more information or google airport the code.} (Update later)
+#'   \code{nycflights13} package for more information or google airport the code.}
 #' \item{air_time}{Amount of time spent in the air.}
 #' \item{distance}{Distance flown.}
 #' }
