2f15ced89d2275c2205ab6a9c13b29ae42708db2
andrewpbray <andrew.bray@gmail.com>
2017-10-09 22:09:25 -0700

    initial commit

---
+++ R/millenials.R
@@ -0,0 +1,10 @@
+#' Millenials and Marriage
+#'
+#' Results from a 2014 Pew poll of a random sample of 75 millenials (Americans born after 1980).
+#'
+#' @format A tbl_df with with 75 obs on 2 variables
+#' \describe{
+#'   \item{id}{a subject identifier}
+#'   \item{response}{response to the question: "Do you favor same-sex marriage?"}
+#'  }
+"millenials"
