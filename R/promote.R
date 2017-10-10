66216a825e16296620d91cce58c7655a18c32ff8
andrewpbray <andrew.bray@gmail.com>
2017-10-09 22:02:44 -0700

    initial commit

---
+++ R/promote.R
@@ -0,0 +1,9 @@
+#' Results from study on sex and promotion decisions
+#'
+#' @format A tbl_df with with 48 obs on 3 variables
+#' \describe{
+#'   \item{id}{a subject identifier}
+#'   \item{sex}{The sex commonly associated with the name on the application}
+#'   \item{decision}{the decision of the bank manager to promote or not promote}
+#'  }
+"promote"
