2b37cee244a056a161558ff4ed2a621f75e5eb6a
andrewpbray <andrew.bray@gmail.com>
2017-10-09 21:50:24 -0700

    initial commit

---
+++ R/yawn.R
@@ -0,0 +1,9 @@
+#' Results from MythBusters: Is Yawning Contagious?
+#'
+#' @format A tbl_df with with 50 obs on 3 variables
+#' \describe{
+#'   \item{id}{an identifier for which province the city in question is in}
+#'   \item{group}{Which city the vote counts are for. Unique in combination with province.}
+#'   \item{response}{the number of legitimate votes cast for Mahmoud Ahmadinejad}
+#'  }
+"yawn"
