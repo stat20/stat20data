d16c6c7d74adced6a757ffae3ef2640f9390b74f
andrewpbray <andrew.bray@gmail.com>
2017-10-10 09:50:43 -0700

    initial commit

---
+++ R/curry.R
@@ -0,0 +1,9 @@
+#' Curry long shots
+#'
+#' In the 2016 season, it was noted that professional basketball player Steph Curry remarkable basket-shooting performance beyond 30 feet.  This data frame contains his long range shooting performance across 27 attempts. For reference the long range shooting percentage of NBA players that season was 7.5%.
+#'
+#' @format A tbl_df with with 27 obs on 1 variables
+#' \describe{
+#'   \item{shot}{the result of a basket attempt, either "hit" or "miss"}
+#'  }
+"curry"
