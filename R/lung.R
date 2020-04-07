67e64c4f2efddf9f8b151b1c9fa378bff796e376
Andrew Bray <andrew.bray@gmail.com>
2020-04-07 09:22:26 -0700

    initial commit

---
+++ R/lung.R
@@ -0,0 +1,9 @@
+#' Lung Disease and Therapy
+#'
+#'
+#' @format A tbl_df with 146 rows and 2 columns:
+#' \describe{
+#'   \item{IQ}{IQ score}
+#'   \item{group}{treatment group to which the subject was assigned, either \code{dexamethasone} or \code{placebo}}.
+#'   }
+"lung"
