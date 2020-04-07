6e30a5fa80f10c88a1c7eea3587b60f16009e971
Andrew Bray <andrew.bray@gmail.com>
2020-04-07 09:37:27 -0700

    initial commit

---
+++ R/cancer.R
@@ -0,0 +1,7 @@
+#' Cancer and height
+#'
+#' 
+#' @format A tbl_df with 1250 rows and 1 column:
+#' \describe{
+#'   \item{developed_cancer}{whether the respondent developed cancer. Either \code{yes} or \code{no}}.
+"cancer"
---
+++ R/nypd.R
@@ -0,0 +1,11 @@
+#' NYPD
+#'
+#' Data concerning stop and frisk policies conducted by Quinnipiac 2013.
+#' 
+#' @format A tbl_df with 902 rows and 2 columns:
+#' \describe{
+#'   \item{answer}{whether the respondent supports the policy of stop and frisk. Either \code{approve} or \code{disapprove}}.
+#'   \item{age_group}{the age group to which the subject belongs. Either \code{"18-34"
+#'   }, \code{"35-54"}, \code{"55+"}.
+#'   }
+"nypd"
