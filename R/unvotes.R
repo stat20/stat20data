11b43770d1b16a9e4b71ef1d7b145b8ce9ab0b58
andrewpbray <andrewbray@berkeley.edu>
2024-08-27 20:47:29 -0700

    add unvotes

---
+++ R/unvotes.R
@@ -0,0 +1,13 @@
+#' Votes at the United Nations
+#'
+#' Country-level voting results on every UN resolution concerning three issues: Colonialism,
+#' Human rights, and Nuclear weapons from 1946 - 2019.
+#'
+#' @format A tbl_df with with 419966 obs on 15 variables, three of which include...
+#' \describe{
+#'   \item{rcid}{the roll call id}
+#'   \item{country}{the country name}
+#'   \item{year}{the year of the vote}
+#'   \item{issue}{the general issue of the resolution}
+#'  }
+"unvotes"
